7ea2cae06ff04187a38af321da698f3817367b5d
mastef <markus.stefanko@gmail.com>
2024-10-01 11:27:54 +0700

    String library

---
+++ lib/strings/strings.ts
@@ -0,0 +1,40 @@
+﻿"use strict";
+
+// commands
+export const reload_popup_controls = "reload_popup_controls";
+export const update_tab_count = "update_tab_count";
+export const discard_tabs = "discard_tabs";
+export const move_tabs_to_window = "move_tabs_to_window";
+export const focus_on_tab_and_window = "focus_on_tab_and_window";
+export const focus_on_tab_and_window_delayed = "focus_on_tab_and_window_delayed";
+export const focus_on_window = "focus_on_window";
+export const focus_on_window_delayed = "focus_on_window_delayed";
+export const set_window_color = "set_window_color";
+export const set_window_name = "set_window_name";
+export const create_window_with_tabs = "create_window_with_tabs";
+export const create_window_with_session_tabs = "create_window_with_session_tabs";
+export const close_tabs = "close_tabs";
+export const switch_to_previous_active_tab = "switch_to_previous_active_tab";
+export const refresh_windows = "refresh_windows";
+
+// context menues
+export const open_in_own_tab = "open_in_own_tab";
+export const open_popup = "open_popup";
+export const open_sidebar = "open_sidebar";
+export const sep1 = "sep1";
+export const support_menu = "support_menu";
+export const review = "review";
+export const donate = "donate";
+export const patron = "patron";
+export const twitter = "twitter";
+export const code_menu = "code_menu";
+export const changelog = "changelog";
+export const options = "options";
+export const source = "source";
+export const report = "report";
+export const send = "send";
+
+// storage keys
+export const windowHashes = "windowHashes";
+export const windowColors = "windowColors";
+export const windowNames = "windowNames";
