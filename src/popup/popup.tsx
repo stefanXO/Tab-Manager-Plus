a985d547af8b3f6a83958f6ca9f4a113a2895d9c
mastef <markus.stefanko@gmail.com>
2026-09-22 19:56:44 +0700

    try/catch

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -40,53 +40,60 @@ setTimeout(loadApp, 15000);
 async function loadApp() {
 	if (!!window.loaded) return;
 	if (!!window.loading) return;
-	window.loading = true;
-	let height : number = await getLocalStorage("tabHeight", 600);
-	let width : number = await getLocalStorage("tabWidth", 800);
-	console.log(height, width);
-	if (window.inPopup) {
+	try {
+		window.loading = true;
+		let height : number = await getLocalStorage("tabHeight", 600);
+		let width : number = await getLocalStorage("tabWidth", 800);
+		console.log(height, width);
+		if (window.inPopup) {
 
-		if (height > 0 && width > 0) {
-			document.body.style.width = width + "px";
-			document.body.style.height = height + "px";
-		}
-
-		const _root = document.getElementById("root");
-		if (_root != null) {
-			var _height = parseInt(document.body.style.height.split("px")[0]) || 0;
-			if (_height < 300) {
-				_height = 400;
-				document.body.style.minHeight = _height + "px";
-			} else {
-				_height++;
-				if (_height > 600) _height = 600;
-				document.body.style.minHeight = _height + "px";
+			if (height > 0 && width > 0) {
+				document.body.style.width = width + "px";
+				document.body.style.height = height + "px";
 			}
-		}
-	} else {
-		if (window.inPanel) {
-			document.documentElement.style.maxHeight = "auto";
-			document.documentElement.style.maxWidth = "auto";
-			document.body.style.maxHeight = "auto";
-			document.body.style.maxWidth = "auto";
-		}
-		document.documentElement.style.maxHeight = "100%";
-		document.documentElement.style.maxWidth = "100%";
-		document.documentElement.style.height = "100%";
-		document.documentElement.style.width = "100%";
-		document.body.style.maxHeight = "100%";
-		document.body.style.maxWidth = "100%";
-		document.body.style.height = "100%";
-		document.body.style.width = "100%";
-	}
 
-	if (!!window.loaded) return;
-	window.loaded = true;
-	const container = document.getElementById('TMP');
-	const root = createRoot(container!);
-	root.render(
-		<TabManager optionsActive={!!window.optionPage}/>
-	);
+			const _root = document.getElementById("root");
+			if (_root != null) {
+				var _height = parseInt(document.body.style.height.split("px")[0]) || 0;
+				if (_height < 300) {
+					_height = 400;
+					document.body.style.minHeight = _height + "px";
+				} else {
+					_height++;
+					if (_height > 600) _height = 600;
+					document.body.style.minHeight = _height + "px";
+				}
+			}
+		} else {
+			if (window.inPanel) {
+				document.documentElement.style.maxHeight = "auto";
+				document.documentElement.style.maxWidth = "auto";
+				document.body.style.maxHeight = "auto";
+				document.body.style.maxWidth = "auto";
+			}
+			document.documentElement.style.maxHeight = "100%";
+			document.documentElement.style.maxWidth = "100%";
+			document.documentElement.style.height = "100%";
+			document.documentElement.style.width = "100%";
+			document.body.style.maxHeight = "100%";
+			document.body.style.maxWidth = "100%";
+			document.body.style.height = "100%";
+			document.body.style.width = "100%";
+		}
+
+		if (!!window.loaded) return;
+		window.loaded = true;
+
+		const container = document.getElementById('TMP');
+		const root = createRoot(container!);
+		root.render(
+			<TabManager optionsActive={!!window.optionPage}/>
+		);
+	} catch (err) {
+		console.error(err);
+		window.loading = false;
+		window.loaded = false;
+	}
 }
 
 window.addEventListener("contextmenu", function (e) {
